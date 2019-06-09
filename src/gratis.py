fe862e4c961ae26724fb34c54bbb8b26ec8fa696
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2019-06-09 07:33:31 +0700

    update sms gratis

--- src/gratis.py
+++ src/gratis.py
@@ -1,5 +1,5 @@
 try:
-	import os,sys,time,mechanize,random
+	import os,sys,time,mechanize,random,requests,re
 	from bs4 import BeautifulSoup as BS
 except ModuleNotFoundError as mdl:
 	exit('Module Err: %s'%(mdl))
@@ -18,12 +18,16 @@ class Gratis:
 		self.banner()
 
 	def banner(self):
+		html=requests.get('https://www.sms-gratis.xyz').text
+		tes=re.findall(r'</b><br>(.*?)</center>',html)
+		tess=str(tes).replace('<font color=green>','').replace('</font>','').replace('<font color=red>','').replace('<br><br>\\t','')
 		print("""
 		;;;;;;;;;;;;;;;;;;;
 		;   Sms Gratis    ;
 		; By: Kang-Newbie ;
 		;;;;;;;;;;;;;;;;;;;
 		""")
+		print("[!] Status Server: "+tess)
 		self.no=input('[?] Nomor Target: ')
 		self.msg=input('[Note] Pesan minimal 5 karakter, maksimal 100 karakter\n[?] Pesan: ')
 		jum=int(input('[?] Jumlah: '))

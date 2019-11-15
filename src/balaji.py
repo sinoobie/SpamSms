be6f2bacb47373c308068c46b5385cd42aee3d68
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2019-11-15 20:16:49 +0700

    Add files via upload

--- src/balaji.py
+++ src/balaji.py
@@ -2,7 +2,7 @@ import os, sys, json, requests, time
 
 class Bala:
 	def __init__(self):
-		self.u="https://api.cloud.altbalaji.com/accounts/mobile/verify?domain=IN"
+		self.u="https://api.cloud.altbalaji.com/accounts/mobile/verify?domain=ID"
 		self.unum()
 
 	def unum(self):
@@ -15,10 +15,22 @@ class Bala:
 				print(f"{i+1}. Success")
 			else:
 				print(f"{i+1}. Failed\n- Detail: {res}")
+			time.sleep(1)
 
 	def send(self,kod,nom):
 		ata={"country_code":kod,"phone_number":nom}
-		req=requests.post(self.u,data=json.dumps(ata))
+		head={"Connection":"keep-alive",
+			"Content-Length":f"{len(str(ata))}",
+			"Accept":"application/json, text/plain, */*",
+			"Origin":"https://lite.altbalaji.com",
+			"Save-Data":"on",
+			"User-Agent":"Mozilla/5.0 (Linux; Android 8.1.0; vivo 1718) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.89 Mobile Safari/537.36",
+			"Content-Type":"application/json;charset=UTF-8",
+			"Referer":"https://lite.altbalaji.com/subscribe?progress=input",
+			"Accept-Encoding":"gzip, deflate, br",
+			"Accept-Language":"en-IN,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,hi;q=0.6",
+			}
+		req=requests.post(self.u,data=json.dumps(ata),headers=head)
 		return req.text
 
 try:
